9da5adb309ba376f2e0124acb2bc6fafcb5b7b77
Américo Baez <75997564@continental.edu.pe>
2025-04-05 22:58:36 -0500

    Update script.js

--- BAEZ/script.js
+++ BAEZ/script.js
@@ -1,14 +1,27 @@
-// Script para el filtro de proyectos
-document.getElementById("search").addEventListener("input", function(e) {
-    const searchTerm = e.target.value.toLowerCase();
-    const proyectos = document.querySelectorAll("#proyectos-lista li");
+// Script para agregar la clase 'selected' al enlace clickeado
+document.querySelectorAll('#sidebar #menu li a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        // Primero eliminamos la clase 'selected' de todos los enlaces
+        document.querySelectorAll('#sidebar #menu li a').forEach(function(link) {
+            link.classList.remove('selected');
+        });
+
+        // Luego añadimos la clase 'selected' al enlace clickeado
+        link.classList.add('selected');
+    });
+});
+
+// Funcionalidad del buscador de proyectos
+document.getElementById('search').addEventListener('input', function() {
+    var searchQuery = this.value.toLowerCase();
+    var proyectos = document.querySelectorAll('#proyectos-lista li');
 
     proyectos.forEach(function(proyecto) {
-        const proyectoName = proyecto.textContent.toLowerCase();
-        if (proyectoName.includes(searchTerm)) {
-            proyecto.style.display = "list-item";
+        var title = proyecto.querySelector('h3 a').textContent.toLowerCase();
+        if (title.includes(searchQuery)) {
+            proyecto.style.display = 'block';
         } else {
-            proyecto.style.display = "none";
+            proyecto.style.display = 'none';
         }
     });
 });
