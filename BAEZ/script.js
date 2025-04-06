dd6e028f7f477b03c020cfb11bd069c65aff624c
Américo Baez <75997564@continental.edu.pe>
2025-04-06 00:03:39 -0500

    Update script.js

--- BAEZ/script.js
+++ BAEZ/script.js
@@ -1,12 +1,10 @@
 // Script para agregar la clase 'selected' al enlace clickeado
 document.querySelectorAll('#sidebar #menu li a').forEach(function(link) {
     link.addEventListener('click', function() {
-        // Primero eliminamos la clase 'selected' de todos los enlaces
         document.querySelectorAll('#sidebar #menu li a').forEach(function(link) {
             link.classList.remove('selected');
         });
-
-        // Luego añadimos la clase 'selected' al enlace clickeado
+        
         link.classList.add('selected');
     });
 });
