db2b592cddcb77676c3133e33975b93384d38e36
aldana38 <74072758@continental.edu.pe>
2025-04-26 16:18:54 -0500

    Create test_script.js
    Creando el archivo test para realizar mis pruebas TDD

---
+++ develop/ALDANA/test_script.js
@@ -0,0 +1,4 @@
+console.assert(sumar(2, 2) === 4, "Error en suma 2+2");
+console.assert(sumar(5, 3) === 8, "Error en suma 5+3");
+console.assert(sumar(0, 0) === 0, "Error en suma 0+0");
+console.log("Todas las pruebas pasaron correctamente.");
